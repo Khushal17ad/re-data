35f4e69db66478aca5f6c06b72d5d9c71a2c1744
Khushal Adlakha <30299004+Khushal17ad@users.noreply.github.com>
2020-11-23 17:58:38 +0100

    Update analysis.py

--- analysis.py
+++ analysis.py
@@ -1,9 +1,11 @@
 import os
 import pandas as pd
 from geopy.geocoders import Nominatim
+from geopy.extra.rate_limiter import RateLimiter
 
 
 def convert_location_to_coords(name):
+    geocode = RateLimiter(locator.geocode, min_delay_seconds=1)
 
     name = name.split(',')[0]
     geolocator = Nominatim(user_agent="Your_Name")
@@ -16,7 +18,9 @@ def convert_location_to_coords(name):
 def get_data():
     norway_data = pd.read_csv('./data/Vindkraftverk.csv',engine='python',error_bad_lines=False, sep = ';')
 
-    norway_data['location'] = norway_data['Fylke(r)'].apply(lambda x: convert_location_to_coords(x))
+    geocode = RateLimiter(locator.geocode, min_delay_seconds=1)
+    norway_data['location'] = norway_data['Fylke(r)'].apply(geocode)
+    #norway_data['location'] = norway_data['Fylke(r)'].apply(lambda x: convert_location_to_coords(x))
     return norway_data
 
 
