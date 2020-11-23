ff031b575f3d51525d9d84e7925dfa6ac3c7db52
Khushal Adlakha <30299004+Khushal17ad@users.noreply.github.com>
2020-11-23 18:34:47 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,7 +18,7 @@ def main():
     #st.table(norway_data)
 
     fig = px.scatter_mapbox(norway_data, lat="lat", lon="lon", hover_name="Fylke(r)", hover_data=["Kraftverksnavn"],
-                        color_discrete_sequence=["fuchsia"], zoom=3, height=300)
+                        color_discrete_sequence=["fuchsia"], zoom=3, height=300, size_max = 40)
     fig.update_layout(mapbox_style="open-street-map")
     fig.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
     fig.show()
