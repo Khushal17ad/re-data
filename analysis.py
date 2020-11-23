9fbf52ea95848f69923a37c04227fdf5b779e4dc
Khushal Adlakha <30299004+Khushal17ad@users.noreply.github.com>
2020-11-23 17:44:30 +0100

    Update analysis.py

--- analysis.py
+++ analysis.py
@@ -8,12 +8,14 @@ def convert_location_to_coords(name):
     name = name.split(',')[0]
     geolocator = Nominatim(user_agent="Your_Name")
     location = geolocator.geocode(name)
-    return location
+
+    lat = location.latitude
+    long = location.longitude
+    return location,lat,long
 
 def get_data():
     norway_data = pd.read_csv('./data/Vindkraftverk.csv',engine='python',error_bad_lines=False, sep = ';')
 
-    
     norway_data['location'] = norway_data['Fylke(r)'].apply(lambda x: convert_location_to_coords(x))
     return norway_data
 
