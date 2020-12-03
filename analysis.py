2b4b653a08ec99df3e8fc11305ce8e85e4dd3f3c
Khushal Adlakha <30299004+Khushal17ad@users.noreply.github.com>
2020-12-03 17:27:32 +0100

    Update analysis.py

--- analysis.py
+++ analysis.py
@@ -30,7 +30,7 @@ def get_data_old():
     norway_data['lon'] = norway_data['point'].apply(lambda x : x[1] if x else None)
 
     
-    column_names = ['year_commissioned','installed_pwoer_total_MW','main_owner','owner_org_number','county',
+    column_names = ['power','year_commissioned','installed_power_total_MW','main_owner','owner_org_number','county',
                 'kommune','avg_annual_prodcution_GWH','avg_generation_power_MW','avg_hub_height_M','avg_rotor_diameter_M',
                 'energy_per_swept_area_KWH_m2','operating_turbines','location','point','lat','lon']
 
