70ed33470f36fd8c7e7abbf23c8f08c00a57b0b2
Khushal Adlakha <30299004+Khushal17ad@users.noreply.github.com>
2020-12-03 17:41:05 +0100

    Update analysis.py

--- analysis.py
+++ analysis.py
@@ -35,7 +35,7 @@ def get_data_old():
                 'kommune','avg_annual_prodcution_GWH','avg_generation_power_MW','avg_hub_height_M','avg_rotor_diameter_M',
                 'energy_per_swept_area_KWH_m2','operating_turbines','location','point','lat','lon']
 
-    power_plant_name
+    
     norway_data.columns = column_names
 
     #norway_data[['latitude', 'longitude', 'altitude']] = pd.DataFrame(norway_data['point'].tolist()) #, index=norway_data.index
