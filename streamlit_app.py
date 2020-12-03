0042ee10b9b53d46117d96504835329f19198901
Khushal Adlakha <30299004+Khushal17ad@users.noreply.github.com>
2020-12-03 16:41:28 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,7 +12,7 @@ import os
 import analysis
 
 def main():
-    norway_data = analysis.get_data()
+    norway_data = analysis.get_data_old()
     
     #print (norway_data)
     st.table(norway_data)
