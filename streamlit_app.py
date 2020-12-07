03ef78025e44335c08e51a5b10a20b2e053848b9
Khushal Adlakha <30299004+Khushal17ad@users.noreply.github.com>
2020-12-07 08:47:40 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,7 +18,7 @@ def main():
     st.table(norway_data)
 
     fig = px.scatter_mapbox(norway_data, lat="lat", lon="lon", hover_name="kommune", hover_data=['year_commissioned'],color = 'avg_annual_prodcution_GWH',
-                        color_continuous_scale='Inferno', zoom=3, height=300, size_max = 10)
+                        color_continuous_scale=['Inferno'], zoom=3, height=300, size_max = 10)
     fig.update_layout(mapbox_style="open-street-map")
     fig.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
     fig.show()
