dbf0cc080d4d39597e00c9b23e5f7a5221d14d77
Khushal Adlakha <30299004+Khushal17ad@users.noreply.github.com>
2021-01-12 19:36:07 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,7 +23,7 @@ def main():
         st.table(norway_data)
 
         norway_data = norway_data.sort_values(by = 'avg_annual_prodcution_GWH')
-        fig = px.scatter_mapbox(norway_data, lat="lat", lon="lon", hover_name="county", hover_data=['main_owner','year_commissioned','operating_turbines'],
+        fig = px.scatter_mapbox(norway_data, lat="lat", lon="lon", hover_name="county", hover_data=['main_owner','operational_year','operating_turbines'],
                             size_max = 10, zoom=3, height=500 ) #color_continuous_scale = px.colors.sequential.Magenta color = 'avg_annual_prodcution_GWH',size = 'year_commissioned',
         fig.update_layout(mapbox_style="open-street-map")
         fig.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
@@ -36,7 +36,7 @@ def main():
 
         st.table(us_data.head())
 
-        fig = px.scatter_mapbox(us_data, lat="lat", lon="lon", hover_name="county", hover_data=['case_id'],
+        fig = px.scatter_mapbox(us_data, lat="lat", lon="lon", hover_name="county", hover_data=['operating_turbines'],
                             size_max = 10, zoom=3, height=500 ) #color_continuous_scale = px.colors.sequential.Magenta color = 'avg_annual_prodcution_GWH',size = 'year_commissioned',
         fig.update_layout(mapbox_style="open-street-map")
         fig.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
@@ -57,7 +57,7 @@ def main():
         #st.table(all_data.head())
         #st.table(all_data.tail())
 
-        fig = px.scatter_mapbox(all_data, lat="lat", lon="lon", hover_name="county", hover_data=['case_id'],
+        fig = px.scatter_mapbox(all_data, lat="lat", lon="lon", hover_name="county", hover_data=['operating_turbines'],
                             size_max = 10, zoom=1, height=800, width = 800 ) #color_continuous_scale = px.colors.sequential.Magenta color = 'avg_annual_prodcution_GWH',size = 'year_commissioned',
         fig.update_layout(mapbox_style="open-street-map")
         fig.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
