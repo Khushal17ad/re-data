8c2014849c03498a1d3d5fdfb5cfd505c7675b1b
Khushal Adlakha <30299004+Khushal17ad@users.noreply.github.com>
2021-01-12 21:21:18 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -70,13 +70,21 @@ def main():
 
         st.subheader('Year Wise Comparison')
 
-        
+        if property_option == 'avg_hub_height_M':
+            property_option_label = "Average Hub Height (in metres)"
+            fig1 = px.scatter(norway_data, x = 'operational_year', y = property_option, labels={
+                        "operational_year" : "Operational Year",
+                        property_option : property_option_label
+                    })
+            st.plotly_chart(fig1)
+        else:
+            property_option_label = "Average Rotor Diameter (in metres)"
 
-        fig1 = px.scatter(us_data, x='operational_year', y='avg_hub_height_M',labels={
-                     "operational_year": "Operational Year",
-                     "avg_hub_height_M": "Average Hub Height (in metres)"
-                 })
-        st.plotly_chart(fig1)
+            fig1 = px.scatter(norway_data, x='operational_year', y=property_option, labels={
+                        "operational_year" : "Operational Year",
+                        property_option : property_option_label
+                    })
+            st.plotly_chart(fig1)
 
     elif country_option == 'All':
         norway_data = analysis.get_norway_data()
