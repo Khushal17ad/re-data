2ad39eee8fcd11632b6169c9ea87a17a6250efb4
Khushal Adlakha <30299004+Khushal17ad@users.noreply.github.com>
2021-01-18 16:24:51 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -117,5 +117,10 @@ def main():
 
     else:
         pass
+
+    st.text("The data visualized in this web app is referenced from multiple data sources")
+    st.markdown(
+    """<a href="https://www.hackerearth.com/challenges/competitive/shell-hackathon/?utm_source=shell&utm_medium=redirect&utm_content=target&utm-campaign=signupIconlistControl&utm_term=ExpA">Shell AI</a>""", unsafe_allow_html=True,
+    )
 if __name__ == "__main__":
     main()
